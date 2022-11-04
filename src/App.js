05068378523901028ab368072651621a8e8c8f86
helloleesul <suerish.e@gmail.com>
2022-11-05 03:12:24 +0900

    컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import Potato from "./Potato";
+
+function Potato() {
+  return (
+    <div>
+      <h1>i love potato</h1>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello</h1>
+      <Potato />
     </div>
   );
 }
