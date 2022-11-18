5740092b5b07210e1da33dbbc82ced44c7a6c989
leesul <55569192+helloleesul@users.noreply.github.com>
2022-11-18 16:19:37 +0900

    scss 설치 style 셋팅

--- src/Movie.js
+++ src/Movie.js
@@ -2,15 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 Movie.propTypes = {
-  id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
 };
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+function Movie({ year, title, summary, poster }) {
+  return (
+    <div className="movie__data">
+      <img src={poster} alt={title} title={title} />
+      <h3 className="movie__title">{title}</h3>
+      <h5 className="movie__year">{year}</h5>
+      <p className="movie__summary">{summary}</p>
+    </div>
+  );
 }
 
 export default Movie;
